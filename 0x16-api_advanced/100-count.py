2c4cd46985b6d61d8c14c619a2e3d03c889fa416
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-11-09 08:35:54 -0800

    changes

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -14,7 +14,7 @@ def count_words(subreddit, word_list, hot_list=[], after=None):
     response = requests.get(
         "https://www.reddit.com/r/{}/hot.json".format(subreddit),
         headers={
-            "User-Agent": "subscribercheck",
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)",
         },
         params=params,
         allow_redirects=False,
