23a37790831a698cab7a082bb2907c0c158e9d3b
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-11-16 17:36:25 -0800

    changes

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,11 @@
-# Resolve huge amount of request
+# Resolve huge amount or request
 
-exec { 'replaces':
-  provider  => shell,
-  command   => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
-  before    => Exec['replaces'],
+exec {'os-config':
+  command => "sed -i '/holberton hard/s/5/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin/'
 }
 
-exec {'replace-1':
-  provider   => shell,
-  command    => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+exec { 'os-config1':
+  command => "sed -i '/holberton soft/s/4/50000/' /etc/security/limits.conf",
+  path    => '/usr/local/bin/:/bin/'
 }
