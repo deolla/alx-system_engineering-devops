c6ffdd9d270e5f56991863320986009ff5064b72
Adeosun Oluwaseyi <deosundeola@gmail.com>
2023-11-16 17:23:57 -0800

    changes

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,13 @@
 # This manuscript increases the amount of traffic an Nginx server can handle
 
-# Increase the ULIMIT of the default file
-file { 'fix-for-nginx':
+file { 'resolve-request':
   ensure  => 'file',
   path    => '/etc/default/nginx',
   content => inline_template('<%= File.read("/etc/default/nginx").gsub(/15/, "4096") %>'),
 }
 
-# Restart Nginx
+# Start/Restart Nginx
 -> exec { 'nginx-restart':
-  command => 'nginx restart',
+  command => 'sudo service nginx restart',
   path    => '/etc/init.d/',
 }
